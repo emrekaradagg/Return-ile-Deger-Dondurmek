6e2601d9f8148b050998d31c4857829219723d86
emre <emre>
2024-03-17 00:34:16 +0300

    Retun

---
+++ app.js
@@ -0,0 +1,30 @@
+//Geriye değer döndüren metot tanımlamak --- return : geriye döndürmek
+
+// let dönenDeger = cube(2);
+// kareAl(dönenDeger);
+
+
+// function kareAl(sayi) {
+//     let islem = sayi*sayi;
+//     console.log(islem);
+// }
+
+/* 
+        RETURN
+    1- Bir değeri metodun dışarısına çıkarmak , taşımak için kullanılır.
+    2- Bir metodu bitiren anahtat kelimedir.
+
+
+*/
+
+// function cube(sayi) {
+//     let sonuc = sayi*sayi*sayi;
+//     return sonuc;
+// }
+
+// function yazdir() {
+//     console.log("Emre");
+//     // void : Geriye değer döndürmeyen demektir.
+// }
+
+// yazdir();
